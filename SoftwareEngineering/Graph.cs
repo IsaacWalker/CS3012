f0d953da29fdaab68c7d512baede3b8aa1b229e7
IsaacWalker <isaacwalker1377@gmail.com>
2018-09-19 20:02:32 +0100

    Graph interface defined

---
+++ SoftwareEngineering/Graph.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareEngineering
+{
+    class Graph
+    {
+        private IList<GraphNode> Nodes = new List<GraphNode>();
+
+
+
+        class GraphNode
+        {
+            readonly string Data;
+
+            IList<GraphNode> Connections = new List<GraphNode>();
+
+            GraphNode(string Data)
+            {
+                this.Data = Data;
+            }
+
+            void AddConnection(GraphNode Node)
+            {
+                Connections.Add(Node);
+            }
+
+        }
+
+    }
+}
---
+++ SoftwareEngineering/IGraph.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareEngineering
+{
+    /// <summary>
+    /// Interface for a directed graph
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    interface IGraph<T>
+    {
+        bool Contains(T Data);
+
+        void Remove(T Data);
+
+        IList<T> GetConnections(T DataNode)
+    }
+}
